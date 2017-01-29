1b9e7019630c8298f42dc2f63294fd135b8d65e2
Ben <ben@bkdev.co.uk>
2017-01-29 20:41:19 +0000

    Fleshed out Settings, added Avatar and permissions

--- app/views/settings/settings.js
+++ app/views/settings/settings.js
@@ -2,27 +2,59 @@ var frameModule = require("ui/frame");
 var appSettings = require("application-settings");
 var view = require("ui/core/view");
 var dialogs = require("ui/dialogs");
+var imageModule = require("image-source");
+var fs = require("file-system");
+var imagepicker = require("nativescript-imagepicker");
+var loadingIndicator = require("nativescript-loading-indicator").LoadingIndicator;
+var http = require("http");
+var permissions = require( "nativescript-permissions" );
+
+var loader = new loadingIndicator;
+var loaderOptions = {
+    message: 'Loading...',
+    progress: 0,
+    android: {
+        indeterminate: true,
+        cancelable: false,
+        max: 100,
+        progressNumberFormat: "%1d/%2d",
+        progressPercentFormat: 0.53,
+        progressStyle: 1,
+        secondaryProgress: 1
+    },
+    ios: {
+        details: "Please wait",
+        square: false,
+        margin: 10,
+        dimBackground:true,
+        color: "#4b9ed6"
+    }
+};
+
+loader.show(loaderOptions);
 
 var drawer;
-
-var settings = [
-    ["toggleableItem", "boolean"]
-];
-settings.forEach(function(e){
-    if(appSettings.hasKey(e[0])) {
-        if(e[1]=="boolean"){
-            settings[0][2] = appSettings.getBoolean(e[0]);
-        }
-    }
-})
+var imgLocal;
 
 exports.loaded = function(args){
+    var username = appSettings.getString("username");
+    //var img = appSettings.hasKey("tmpImg") ? appSettings.getString("tmpImg") : appSettings.getString("img");
+    var img = appSettings.getString("img");
+
     var page = args.object;
     page.bindingContext = {
-        "toggleableItem" : settings[0][2]
+        "username" : username,
+        "img" : img
     };
     drawer = view.getViewById(page,"sideDrawer");
-    console.log("Settings page successfully");
+
+    var usernameField = view.getViewById(page,'username');
+        var fArray = [];
+        fArray[0] = new android.text.InputFilter.LengthFilter(16);
+        usernameField.android.setFilters(fArray);
+
+    loader.hide();
+    console.log("Settings page successfully loaded");
 }
 
 exports.toggleDrawer = toggleDrawer;
@@ -38,8 +70,26 @@ function navDashboard(){
 exports.save = save;
 function save(args){
     var page = args.object.bindingContext;
-    appSettings.setBoolean("toggleableItem", page["toggleableItem"]);
+    //appSettings.setBoolean("toggleableItem", page["toggleableItem"]);
+    var n_username = page["username"];
+
     console.log("Settings saved");
+}
+
+exports.changeImg = changeImg;
+function changeImg(args){
+    var page = args.object;
+    console.log("Image change selected");
+    permissions.requestPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, "We need this permission to allow us to save your new Avatar to your profile")
+        .then(function(){
+            permiss = true;
+            console.log("Permissions accepted");
+            //Do image replacement
+        })
+        .catch(function(){
+            console.log("Permissions denied");
+        });
+
 }
 
 exports.logout = logout;
